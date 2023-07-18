f958805a3a364cb665b2785bd6fbacee9754bc8e
olegmaslov <oleg.gnome@yandex.ru>
2023-07-18 10:43:04 +0300

    fixed 6

--- blog/tasks.py
+++ blog/tasks.py
@@ -1,6 +1,10 @@
+import datetime
+
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
+
+from blog.models import Like
 
 
 @shared_task
@@ -8,6 +12,18 @@ def send_moderator_email():
     send_mail(
         subject='Новый комментарий',
         message='В блоге появился новый комментарий, необходимо провести ревью',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[settings.EMAIL_MODERATOR]
+    )
+
+
+@shared_task
+def send_moderator_likes_count():
+    likes_count = Like.objects.all().count()
+    now = datetime.datetime.now()
+    send_mail(
+        subject='Количество лайков',
+        message=f'На {now} количество лайков: {likes_count}',
         from_email=settings.EMAIL_HOST_USER,
         recipient_list=[settings.EMAIL_MODERATOR]
     )
--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -153,3 +154,10 @@ EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
 EMAIL_MODERATOR = os.getenv('EMAIL_MODERATOR')
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
+
+CELERY_BEAT_SCHEDULE = {
+    'likes-count': {
+        'task': 'blog.tasks.send_moderator_likes_count',
+        'schedule': timedelta(days=1),
+    },
+}
